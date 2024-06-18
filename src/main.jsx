f1efd41910f4cfbe2d0a081e4636cd7ba99c3498
Riley OKeefe <rileyjsix@gmail.com>
2024-06-18 12:52:40 -0300

    Test config

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import Layout from './Layout.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "",
+    path: "/personal-portfolio/",
     element: <Layout />, 
     children: [
       {

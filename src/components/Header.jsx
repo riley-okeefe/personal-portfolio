d0ccb54299b9009f69d09aefb960d7b583e8b990
Riley OKeefe <rileyjsix@gmail.com>
2024-11-05 20:45:14 -0400

    Add mobile navigation menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,31 @@ import "./header.css";
 
 export default function Header() {
     const [activeComponent, setActiveComponent] = useState("");
+    const [menuActive, setMenuActive] = useState(false);
 
     const handleClick = (component) => {
         setActiveComponent(component);
+        setMenuActive(false);
     };
 
     const isActive = (component) => {
         return activeComponent === component ? 'active' : '';
     };
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    }
+
     return (
         <div className="welcome-card">
             <div className="welcome-options">
                 <h1 className='nav-title'>Hi, I'm Riley.</h1>
-                <div className='welcome-option-group'>
+                <div className='hamburger' onClick={toggleMenu}>
+                    <span className={`bar ${menuActive ? 'active': ''}`}></span>
+                    <span className={`bar ${menuActive ? 'active': ''}`}></span>
+                    <span className={`bar ${menuActive ? 'active': ''}`}></span>
+                </div>
+                <div className={`welcome-option-group ${menuActive ? 'active' : ''}`}>
                     <div className="welcome-option">
                         <Link to='/personal-portfolio/' className={isActive("home")} onClick={() => handleClick("home")}>Home</Link>
                     </div>

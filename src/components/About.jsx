f052579411595ceb6f2d688e3c3a3825edf2f9ac
Riley OKeefe <rileyjsix@gmail.com>
2025-11-09 15:01:23 -0400

    Update text content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,25 +7,25 @@ export default function About() {
                 <div className="content-about">
                     <h1>Get to know me</h1>
                     <p>
-                    A Computer Science Graduate from Saint Mary's University, 
-                    posessing multiple skills and certifications in this field, such as AI For 
-                    Everyone from Coursera, Fundamentals of Accelerated Computing with CUDA C/C++ 
-                    from NVIDIA, Neural Networks and Deep Learning from DeepLearning AI on Coursera,
-                    and Intro to DevOps from IBM on Coursera. In addition I have received a Co-Curricular 
-                    Experience Record from Saint Mary's University in which I gained valuable practical 
-                    experience in the areas of Software Engineering and Web Development, along with 
-                    experience using various programming languages.
+                    A Computer Science Graduate from Saint Mary's University, with a strong foundation
+                    in software development, and computer science fundamentals, complemented by certifications
+                    such as <em>AI for Everyone</em> (Coursera), <em>Fundamentals of Accelerated Compututing with CUDA C/C++</em> (NVIDIA), 
+                    <em> Neural Networks and Deep Learning</em> (DeepLearning AI on Coursera), and <em>Introduction to DevOps</em> (IBM on Coursera).
+                    <br/><br/>
+                    Throughout my academic, co-curricular and professional experiences, I have gained practical skills in
+                    building and testing software using a variety of programming languages and frameworks.
+                    I'm particualrly interested in the intersection of software engineering, AI, and cloud technologies.
+                    <br/><br/>
+                    I'm currently working as a <strong>Graduate Software Engineer at Kainos</strong>, where I'm contributing
+                    to a public sector project using the .NET stack. My role thus far has involved developing new features, fixing bugs,
+                    and refactoring existing code with a focus on Test Driven Development.
+                    As part of this role, I am taking part in the Kainos Early Careers program, which has included attending a 3 week Software Engineering Academy
+                    where we learned about industry best practices, agile methodologies, N-tier architecture along with the components of each layer, and many other items. 
+                    To wrap it all together we took part in mock sprints at the end of each week to simulate real world software development scenarios and to reinforce our learning throughout the week.
                     <br/><br/>
                     Whether it's a new programming language, AI/ML tool, library or framework, I'm 
                     always eager to expand my knowledge and I thrive on exploring and learning about 
                     new technologies.
-                    <br/><br/>
-                    Currently I am utilizing my problem solving and analytical thinking skills gained from my Computer Science 
-                    degree working as an IT Analyst. 
-                    <br/><br/>
-                    Through these experiences, I have developed strong competencies in timeline management, 
-                    problem solving, communication, and teamwork which I aim to further enhance and utilize 
-                    in my future career.
                     </p>
                 </div>
                 <div className="content-skills">
@@ -34,10 +34,10 @@ export default function About() {
                         <strong>Programming languages I have experience using:</strong> Python, C, C++, C#, Java, JavaScript,
                         PHP, SQL, CUDA, HTML, and CSS.
                         <br/><br/>
-                        <strong>Frameworks/libraries I have experience using:</strong> Flask, Numpy, React.
+                        <strong>Frameworks/libraries I have experience using:</strong> Flask, Numpy, React, .NET, NUnit.
                         <br/><br/>
-                        <strong>Tools I have experience using:</strong> GitHub, Git, Microsoft 365, Visual Studio, 
-                        Visual Studio Code, MySQL, Active Directory, BMC Remedy SmartIT ticketing system, Beyond Trust 
+                        <strong>Tools I have experience using:</strong> GitHub, Git, GitLab, Microsoft 365, Visual Studio, 
+                        Visual Studio Code, Intellij IDEA, Rider, MySQL, Active Directory, BMC Remedy SmartIT ticketing system, Beyond Trust 
                         secure remote support, ConnectWise (PSA, Automate, ScreenConnect)
                         <br/><br/>
                         <strong>Game engines I have experience using:</strong> Unity.
